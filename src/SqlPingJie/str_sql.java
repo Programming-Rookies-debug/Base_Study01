501cb219c1aae07706136cd35ed5834963c02e04
panleiSun <lei1224311727@aliyun.com>
2021-09-30 15:40:08 +0800

    sql拼接编写

---
+++ src/SqlPingJie/str_sql.java
@@ -0,0 +1,18 @@
+package SqlPingJie;
+
+/**
+ * @author Sunpl
+ * @description 拼接SQL
+ * @date 2021/9/29
+ */
+public class str_sql {
+
+
+    public  static  String   SplicingInsert(){
+        String str="insert into";
+        String table="";
+        StringBuffer  sql=new StringBuffer();
+        sql.append("");
+        return null;
+    }
+}
